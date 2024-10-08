206698a113ef9512f0a28dd1566d4940f64b1228
Rishiraj Thadeshwar <58561350+01STARK@users.noreply.github.com>
2024-10-08 14:26:13 -0400

    Update cat_dog.py

--- String-2/cat_dog.py
+++ String-2/cat_dog.py
@@ -1,1 +1,9 @@
-
+def cat_dog(str):
+  countcat = 0
+  countdog = 0
+  for i in range(len(str)-2):
+    if str[i] == "c" and str[i+1]=="a" and str[i+2]=="t":
+      countcat +=1
+    if str[i] == "d" and str[i+1]=="o" and str[i+2]=="g":
+      countdog +=1
+  return countcat == countdog
